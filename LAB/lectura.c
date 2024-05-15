8ff0b5241a12778a704c38991da4d2f84334993a
Andres Zelaya <andres.zelaya@usach.cl>
2024-05-15 00:10:59 -0400

    Arreglo Makefile

--- LAB/lectura.c
+++ LAB/lectura.c
@@ -5,22 +5,26 @@
 #include "lectura.h"
 
 // Apertura del archivo en modo lectura binaria
-BMPImage* read_bmp(const char* filename) {
-    FILE* file = fopen(filename, "rb"); // rb = read binary
-    if (!file) {
+BMPImage *read_bmp(const char *filename)
+{
+    FILE *file = fopen(filename, "rb"); // rb = read binary
+    if (!file)
+    {
         fprintf(stderr, "Error: No se pudo abrir el archivo.\n");
         return NULL;
     }
 
     // Lectura de la cabecera del archivo BMP
     BMPHeader header;
-    if (fread(&header, sizeof(BMPHeader), 1, file) != 1) {
+    if (fread(&header, sizeof(BMPHeader), 1, file) != 1)
+    {
         fprintf(stderr, "Error: No se pudo leer la cabecera del archivo.\n");
         fclose(file);
         return NULL;
     }
     // Validación de la cabecera del archivo BMP
-    if (header.type != 0x4D42) {
+    if (header.type != 0x4D42)
+    {
         fprintf(stderr, "Error: El archivo no es un BMP válido.\n");
         fclose(file);
         return NULL;
@@ -29,15 +33,17 @@ BMPImage* read_bmp(const char* filename) {
     // Lectura de la información de la imagen BMP
     BMPInfoHeader info_header;
     // fread(puntero a la estructura, tamaño de la estructura, cantidad de estructuras, archivo)
-    if (fread(&info_header, sizeof(BMPInfoHeader), 1, file) != 1) {
+    if (fread(&info_header, sizeof(BMPInfoHeader), 1, file) != 1)
+    {
         fprintf(stderr, "Error: No se pudo leer la información de la imagen.\n");
         fclose(file);
         return NULL;
     }
 
     // Malloc de la estructura BMPImage
-    BMPImage* image = (BMPImage*)malloc(sizeof(BMPImage));
-    if (!image) {
+    BMPImage *image = (BMPImage *)malloc(sizeof(BMPImage));
+    if (!image)
+    {
         fprintf(stderr, "Error: No se pudo asignar memoria para la imagen.\n");
         fclose(file);
         return NULL;
@@ -46,8 +52,9 @@ BMPImage* read_bmp(const char* filename) {
     // Malloc de los píxeles de la imagen
     image->width = info_header.width;
     image->height = info_header.height;
-    image->data = (Pixel*)malloc(sizeof(Pixel) * image->width * image->height);
-    if (!image->data) {
+    image->data = (Pixel *)malloc(sizeof(Pixel) * image->width * image->height);
+    if (!image->data)
+    {
         fprintf(stderr, "Error: No se pudo asignar memoria para los datos de la imagen.\n");
         free(image);
         fclose(file);
@@ -63,10 +70,13 @@ BMPImage* read_bmp(const char* filename) {
     // se hace padding para que la imagen tenga un tamaño múltiplo de 4, esto se hace para que la imagen sea más rápida de leer
     int padding = (4 - (info_header.width * sizeof(Pixel)) % 4) % 4; // primero se pasan a bytes los píxeles de la imagen y se calcula el residuo de la división entre 4, si el residuo es 0 no hay padding, si el residuo es 1, 2 o 3 se calcula el padding
     // Lectura de los píxeles de la imagen
-    for (int y = info_header.height - 1; y >= 0; y--) {
-        for (int x = 0; x < info_header.width; x++) {
+    for (int y = info_header.height - 1; y >= 0; y--)
+    {
+        for (int x = 0; x < info_header.width; x++)
+        {
             Pixel pixel;
-            if (fread(&pixel, sizeof(Pixel), 1, file) != 1) {
+            if (fread(&pixel, sizeof(Pixel), 1, file) != 1)
+            {
                 fprintf(stderr, "Error: No se pudo leer los datos del pixel.\n");
                 free(image->data);
                 free(image);
@@ -83,17 +93,21 @@ BMPImage* read_bmp(const char* filename) {
 }
 
 // Liberación de la memoria de la imagen BMP
-void free_bmp(BMPImage* image) {
-    if (image) {
+void free_bmp(BMPImage *image)
+{
+    if (image)
+    {
         free(image->data);
         free(image);
     }
 }
 
-//funcion para guardar la imagen en un archivo
-void write_bmp(const char* filename, BMPImage* image) {
-    FILE* file = fopen(filename, "wb"); //wb = write binary
-    if (!file) {
+// funcion para guardar la imagen en un archivo
+void write_bmp(const char *filename, BMPImage *image)
+{
+    FILE *file = fopen(filename, "wb"); // wb = write binary
+    if (!file)
+    {
         fprintf(stderr, "Error: No se pudo abrir el archivo.\n");
         return;
     }
@@ -117,8 +131,10 @@ void write_bmp(const char* filename, BMPImage* image) {
     // Padding es el contenido de la imagen en sí
     int padding = (4 - (image->width * sizeof(Pixel)) % 4) % 4;
     // Por cada pixel va a
-    for (int y = image->height - 1; y >= 0; y--) {
-        for (int x = 0; x < image->width; x++) {
+    for (int y = image->height - 1; y >= 0; y--)
+    {
+        for (int x = 0; x < image->width; x++)
+        {
             Pixel pixel = image->data[y * image->width + x];
             fwrite(&pixel, sizeof(Pixel), 1, file);
         }
--- LAB/main.c
+++ LAB/main.c
@@ -8,7 +8,8 @@
 #include "clasificacion.h"
 #include "resultados.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     // Variables para las opciones
     char *nombre_prefijo = NULL;
     int cantidad_filtros = 3;
@@ -23,34 +24,37 @@ int main(int argc, char *argv[]) {
     printf("FUNCIONA\n");
 
     // Process the options
-    while((option = getopt(argc, argv, "N:f:p:u:v:C:R:")) != -1){
-        switch(option){
-            case 'N':
-                nombre_prefijo = optarg;
+    while ((option = getopt(argc, argv, "N:f:p:u:v:C:R:")) != -1)
+    {
+        switch (option)
+        {
+        case 'N':
+            nombre_prefijo = optarg;
+            break;
+        case 'f':
+            if (optarg != NULL)
+            {
+                cantidad_filtros = atoi(optarg);
                 break;
-            case 'f':
-                if(optarg != NULL) {
-                    cantidad_filtros = atoi(optarg);
-                    break;
-                }
-            case 'p':
-                factor_saturacion = atof(optarg);
-                break;
-            case 'u':
-                umbral_binarizacion = atof(optarg);
-                break;
-            case 'v':
-                umbral_clasificacion = atof(optarg);
-                break;
-            case 'C':
-                nombre_carpeta = optarg;
-                break;
-            case 'R':
-                nombre_archivo_csv = optarg;
-                break;
-            default:
-                printf("Opción desconocida: %c\n", option);
-                return 1;
+            }
+        case 'p':
+            factor_saturacion = atof(optarg);
+            break;
+        case 'u':
+            umbral_binarizacion = atof(optarg);
+            break;
+        case 'v':
+            umbral_clasificacion = atof(optarg);
+            break;
+        case 'C':
+            nombre_carpeta = optarg;
+            break;
+        case 'R':
+            nombre_archivo_csv = optarg;
+            break;
+        default:
+            printf("Opción desconocida: %c\n", option);
+            return 1;
         }
     }
 
@@ -62,36 +66,50 @@ int main(int argc, char *argv[]) {
     printf("\n");
     // Ahora se llama a la lectura
     BMPImage *imagen = read_bmp(nombre_archivo);
-    if (imagen == NULL) {
+    if (imagen == NULL)
+    {
         printf("Error al leer la imagen\n");
         return 1;
     }
     printf("Imagen leída\n");
+    // Imprimir la información de la imagen
+    printf("Ancho: %d\n", imagen->width);
+    printf("Alto: %d\n", imagen->height);
 
     // Ahora se aplican los filtros
     char output_filename[256];
 
     // Ahora crear el directorio con los resultados
-    if (create_directory(nombre_carpeta) == 1) {
+    if (create_directory(nombre_carpeta) == 1)
+    {
         printf("Error al crear el directorio\n");
     }
 
-    if (cantidad_filtros  == 1){
+    if (cantidad_filtros == 1)
+    {
         // Saturación
         sprintf(output_filename, "%s/%s", nombre_carpeta, "saturada.bmp");
         BMPImage *imagen_saturada = saturate_bmp(imagen, factor_saturacion);
-        if (imagen_saturada == NULL) {
+        if (imagen_saturada == NULL)
+        {
             printf("Error al saturar la imagen\n");
             return 1;
         }
+        // Imprimir la información de la imagen
+        printf("Ancho: %d\n", imagen_saturada->width);
+        printf("Alto: %d\n", imagen_saturada->height);
+        // Imprimir informacion del header
+
         printf("Imagen saturada\n");
         write_bmp(output_filename, imagen_saturada);
     }
-    if(cantidad_filtros == 2){
+    if (cantidad_filtros == 2)
+    {
         // Saturación
         sprintf(output_filename, "%s/%s", nombre_carpeta, "saturada.bmp");
         BMPImage *imagen_saturada = saturate_bmp(imagen, factor_saturacion);
-        if (imagen_saturada == NULL) {
+        if (imagen_saturada == NULL)
+        {
             printf("Error al saturar la imagen\n");
             return 1;
         }
@@ -101,7 +119,8 @@ int main(int argc, char *argv[]) {
         // Escala de grises
         sprintf(output_filename, "%s/%s", nombre_carpeta, "gris.bmp");
         BMPImage *imagen_gris = grayscale_bmp(imagen);
-        if (imagen_gris == NULL) {
+        if (imagen_gris == NULL)
+        {
             printf("Error al convertir a escala de grises\n");
             return 1;
         }
@@ -109,11 +128,13 @@ int main(int argc, char *argv[]) {
         write_bmp(output_filename, imagen_gris);
     }
 
-    if (cantidad_filtros == 3){
+    if (cantidad_filtros == 3)
+    {
         // Saturación
         sprintf(output_filename, "%s/%s", nombre_carpeta, "saturada.bmp");
         BMPImage *imagen_saturada = saturate_bmp(imagen, factor_saturacion);
-        if (imagen_saturada == NULL) {
+        if (imagen_saturada == NULL)
+        {
             printf("Error al saturar la imagen\n");
             return 1;
         }
@@ -123,7 +144,8 @@ int main(int argc, char *argv[]) {
         // Escala de grises
         sprintf(output_filename, "%s/%s", nombre_carpeta, "gris.bmp");
         BMPImage *imagen_gris = grayscale_bmp(imagen);
-        if (imagen_gris == NULL) {
+        if (imagen_gris == NULL)
+        {
             printf("Error al convertir a escala de grises\n");
             return 1;
         }
@@ -133,7 +155,8 @@ int main(int argc, char *argv[]) {
         // Binarización
         sprintf(output_filename, "%s/%s", nombre_carpeta, "binaria.bmp");
         BMPImage *imagen_binaria = binary_bmp(imagen, umbral_binarizacion);
-        if (imagen_binaria == NULL) {
+        if (imagen_binaria == NULL)
+        {
             printf("Error al binarizar la imagen\n");
             return 1;
         }
@@ -141,21 +164,16 @@ int main(int argc, char *argv[]) {
         write_bmp(output_filename, imagen_binaria);
     }
 
-
     // Ahora clasificación - Se aplica siempre
     bool clasificacion = is_nearly_black(imagen, umbral_clasificacion);
     printf("Clasificación: %d\n", clasificacion);
 
     // Ahora crear el archivo CSV
-    if (create_csv(nombre_archivo_csv, nombre_archivo, clasificacion) == 1) {
+    if (create_csv(nombre_archivo_csv, nombre_archivo, clasificacion) == 1)
+    {
         printf("Error al crear el archivo CSV\n");
     }
     printf("FIN\n");
-
-
-
-
-
 
     return 0;
 }
